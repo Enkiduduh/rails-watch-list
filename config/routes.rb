5a81bf0e9b00aea980ffeb51c8495a93cb1466a9
Ilyes Soumar Djouma <isoumardjouma@reseau.free.fr>
2022-08-18 17:03:21 +0200

    exo

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
+  get 'bookmarks/new'
+  get 'bookmarks/create'
+  get 'bookmarks/destroy'
+  resources :lists, only: [:new, :create, :index, :show] do
+    resources :booksmarks, only: [:new, :create, :destroy]
+  end
+
+
+
+  # get 'lists/index'
+  # get 'lists/show'
+  # get 'lists/create'
+  # get 'lists/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # root to: "lists/index"
 end
