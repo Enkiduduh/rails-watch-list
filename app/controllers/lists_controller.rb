fb0f7e2e1a8bda04e51320f67b62bd68a563c906
Ilyes Soumar Djouma <isoumardjouma@reseau.free.fr>
2022-08-19 11:22:17 +0200

    watch-list suite

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -10,7 +10,7 @@ class ListsController < ApplicationController
   def create
     @list = List.new(list_params)
     if @list.save
-      redirect_to @list
+      redirect_to list_path(@list)
     else
       render :new, status: :unprocessable_entity
     end
@@ -18,6 +18,12 @@ class ListsController < ApplicationController
 
   def new
     @list = List.new
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path, status: :see_other
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,17 @@
 Rails.application.routes.draw do
-  get 'bookmarks/new'
-  get 'bookmarks/create'
-  get 'bookmarks/destroy'
-  resources :lists, only: [:new, :create, :index, :show] do
-    resources :booksmarks, only: [:new, :create, :destroy]
-  end
-
-
-
-  # get 'lists/index'
-  # get 'lists/show'
-  # get 'lists/create'
-  # get 'lists/new'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root to: "lists/index"
+root to: "lists#index"
+resources :lists, except: [:edit, :update] do
+  resources :bookmarks, only: [:new, :create]
+  resources :reviews, only: :create
 end
+resources :bookmarks, only: :destroy
+resources :reviews, only: :destroy
+end
+
+# root to: "lists#index"
+# get 'bookmarks/new'
+# get 'bookmarks/create'
+# get 'bookmarks/destroy'
+# resources :lists, only: [:new, :create, :index, :show] do
+#   resources :booksmarks, only: [:new, :create, :destroy]
+# end
